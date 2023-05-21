9b932df1318bcc47b93e9fe5fb2d7f4eb399111f
Hoang Nhan <hnhan793@gmail.com>
2023-05-21 11:55:49 +0700

    push

---
+++ main.js
@@ -0,0 +1,10 @@
+const musicList = [
+    {
+        'img': '.img/nauchoeman.jpg',
+        'name': 'Nấu Cho Em Ăn',
+        'artist': 'Đen Vâu',
+        'music': './music/NauChoEmAn.mp3'
+    }
+]
+
+musicList.music.play();
